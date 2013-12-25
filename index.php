4dd803ba62fb702a88bbdd6b6522f0638ec2e9a6
ExitIntel-Matt <Matt@ExitIntel.com>
2013-12-24 20:38:35 -0500

    Added in index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Exotic Auto Rentals</title>
+
+<?php include('templates/first/includes/html_head_include_css.php'); ?>
+<link href="templates/first/css/index.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<?php include('templates/first/includes/html_navbar.php'); ?>
+
+	<div class="fp-full-width-header">
+		<div class=container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="fp-hero-container">
+						<h1>Drive the dream, today.</h1>
+						<p>Raleigh &nbsp; | &nbsp; Charlotte &nbsp; | &nbsp; Wilmington</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="bg-orange text-callout-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<p class="p-large p-white text-center">Welcome to Elegant Exotics the premier exotic car rental company in the Carolinas. Feel what it's like to drive the most exclusive vehicles on earth from BMW's to Lamborghini's!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container fleet-container">
+		<div class="row fleet-car-container-row">
+			<div class="col-lg-4">
+				<a href="#">
+					<div class="fleet-car-container">
+						<div class="fleet-car-photo">
+							<img src="templates/first/img/fleet-cover-bmw-m3-red.jpg">
+						</div>
+						<div class="fleet-car-headline">BMW M3</div>
+						<div class="fleet-car-subheadline"></div>
+						<div class="fleet-car-subheadline"></div>
+						<div class="fleet-car-body"></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4">
+				
+			</div>
+			<div class="col-lg-4">
+				
+			</div>
+		</div>
+	</div>
+	<!-- /container -->
+
+	<?php include('templates/first/includes/html_js.php'); ?>
+
+</body>
+</html>
